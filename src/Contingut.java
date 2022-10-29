23a1a5a9dac4a703c69573d493a6b3580c088d5e
mroman <marc.roman.quilez@estudiantat.upc.edu>
2022-10-29 18:03:24 +0200

    Classe contingut

---
+++ src/Contingut.java
@@ -0,0 +1,17 @@
+import java.util.Vector
+
+public class Contingut {
+    private Vector<String> frases;
+
+    private void converteix_a_frases(String fr) {
+
+    }
+
+    public contingut() {
+
+    }
+
+    public contingut(String fr) {
+        converteix_a_frases(fr);
+    }
+}

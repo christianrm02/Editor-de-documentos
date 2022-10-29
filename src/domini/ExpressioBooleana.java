16d78b39c3dab6465a2770decbe3d4c77c161ec1
polfr <pol.fradera@estudiantat.upc.edu>
2022-10-29 11:34:09 +0200

    Creació classe ExpressioBooleana

---
+++ src/domini/ExpressioBooleana.java
@@ -0,0 +1,35 @@
+
+
+public class ExpressioBooleana {
+
+    private String nom;
+    private String exp;
+    private Tree expA;
+
+    //Constructora
+    public ExpressioBooleana (String nom, String exp) {
+        this.nom = nom;
+        this.exp = exp;
+        crearArbre();
+    }
+
+    private crearArbre() {
+
+    }
+
+    //Getters
+    public String getNom() { return nom; }
+
+    public String getExp() { return exp; }
+
+    public Tree getExpA() { return expA; }
+
+
+    //Setters
+    public void setNom(String nom) { this.nom = nom }
+
+    public void setExp(String exp) {
+        this.exp = exp;
+        crearArbre();
+    }
+}

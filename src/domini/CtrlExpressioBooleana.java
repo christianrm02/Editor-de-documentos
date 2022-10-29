4276ecf829ad1e5258f0fcf07623318ef73d6705
polfr <pol.fradera@estudiantat.upc.edu>
2022-10-29 11:02:31 +0200

    Creació CtrlExpressioBooleana amb les funcions buides

---
+++ src/domini/CtrlExpressioBooleana.java
@@ -0,0 +1,20 @@
+import java.util.Vector;
+
+public class CtrlExpressioBooleana {
+    public CtrlExpressioBooleana() {
+
+    }
+
+    public ExpressioBooleana getExpressioBooleana(String expressio) {
+
+    }
+
+    public Boolean existsExpressioBooleana(String expressio) {
+
+    }
+
+    public Vector<ExpressioBooleana> getAll() {
+
+    }
+
+}

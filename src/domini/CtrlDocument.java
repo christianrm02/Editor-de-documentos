53943cbd137063304f2e9770d5f0a53b0d81c6d7
christian.rivero <christian.rivero@estudiantat.upc.edu>
2022-10-30 13:21:03 +0100

    programando ctrlDocument (map de map) + PairAutorTitol

--- src/domini/CtrlDocument.java
+++ src/domini/CtrlDocument.java
@@ -1,62 +1,99 @@
 import domini.Document;
+import domini.PairAutorTitol;
+import java.util.HashMap;
 
 public class CtrlDocument {
     private Document docAct;
-    private vector<Document> documents;
+    private Map<String, Map<String, Document>> documents;
 
 
     /*CONTRUCTORA*/
     public CtrlDocument() {
-
+        documents = new TreeMap<String, Map<String, Document>>();
     }
 
     /*GETTERS*/
     public Document getDocument(String autor, String titol) {
-
+        Document d = documents.get(autor).get(titol);
+        return d;
     }
 
     public boolean existsDocument(String autor, String titol) {
-
+        boolean exists = documents.containsKey(autor).containsKey(titol);
+        return exists;
     }
 
-    public ---<Document> getAll() {
-
+    public vector<Document> getAll() {
+        vector<Document> docs = new Vector();
+        for(Entry<String, Map<String, Document>> titols : documents.entrySet()) {
+            for(Entry<String, Document> document : titols.entrySet()) {
+                docs.add(document);
+            }
+        }
+        return docs;
     }
 
-    public ---<String> getTitols() {
-
+    public Vector<String> getTitols() {
+        vector<String> titols = new Vector();
+        for(Entry<String, Map<String, Document>> titols : documents.entrySet()) {
+            for (Entry<String, Document> document : titols.entrySet()) {
+                docs.add(document.getKey());
+            }
+        }
+        return titols;
     }
 
-    public ---<String> getAutors() {
+        public Vector<String> getAutors() {
+            vector<String> autors = new Vector();
+            for(Entry<String, Map<String, Document>> titols : documents.entrySet()) {
+                docs.add(titols.getKey());
+            }
+            return titols;
+        }
 
-    }
-
-    public ---<String, String> getClaus() {
-
+    public Vector<PairAutorTitol> getClaus() {
+        Vector<PairAutorTitol> claus = new Vector();
+        String autor, titol;
+        for(Entry<String, Map<String, Document>> titols : documents.entrySet()) {
+            autor = titols.getKey();
+            for (Entry<String, Document> document : titols.entrySet()) {
+                titol = document.getKey();
+                c = new PairAutorTitol(autor, titol)
+                claus.add(c);
+            }
+        }
+        return claus;
     }
 
     public String getContingut(String autor, String titol) {
-
+        return documents.get(autor).get(titol).getContingut();
     }
 
     /*SETTERS*/
-    public crearDocument(String autor, String titol) {
+    public crearDocument(String autor, String titol) { ////////////////////
+        if(documents.containsKey(autor).containsKey(titol)) {
+            ----LANZAR EXC YA EXISTE
+        }
+        else if (documents.containsKey(autor)){ //si no existe el autor, si existe el autor pero no el titulo
+            documents.get(autor)
+        }
+    }
+
+    public void esborrarDocuments(Vector<PairAutorTitol> docs) { /////////////
+        for(PairAutorTitol doc : docs) {
+            documents.remove(doc.getAutor()).remove(doc.getTitol());
+        }
+    }
+
+    public void modificarTitol(String autor, String titol, String newT) {
 
     }
 
-    public esborrarDocuments(Set<String, String> docs) {
+    public void modificarAutor(String autor, String titol, String newA) {
 
     }
 
-    public modificarTitol(String autor, String titol, String newT) {
-
-    }
-
-    public modificarAutor(String autor, String titol, String newA) {
-
-    }
-
-    public modificarTContingut(String autor, String titol, String newC) {
+    public void modificarContingut(String autor, String titol, String newC) {
 
     }
 
---
+++ src/domini/PairAutorTitol.java
@@ -0,0 +1,17 @@
+public class PairAutorTitol {
+    private String autor;
+    private String titol;
+
+    public PairAutorTitol(String autor, String titol) {
+        this.autor = autor;
+        this.titol = titol;
+    }
+
+    public getAutor() {
+        return autor;
+    }
+
+    public getTitol() {
+        return titol;
+    }
+}

e2906ae8208507c380f6f0aecb189b88696fe839
polfr <pol.fradera@estudiantat.upc.edu>
2022-11-05 13:31:41 +0100

    canvis a tree

--- src/domini/datatypes/Tree.java
+++ src/domini/datatypes/Tree.java
@@ -1,18 +1,68 @@
 package datatypes;
 import java.util.*;
 
-public class Tree<String> {
-    private Node<String> arrel;
+public class Tree {
 
-    public Tree(String arrelValor) {
-        arrel = new Node<String>();
-        arrel.valor = arrelValor;
-        arrel.fills = new ArrayList<Node<String>>();
+    private TreeNode root;
+
+    public Tree() {
+        this.root = null;
     }
 
-    public static class Node<String> {
-        private String valor;
-        private Node<String> pare;
-        private List<Node<String>> fills;
+    public void insert(String data) {
+        if (root == null) root = new TreeNode(data);
+        else {
+            //insertar al lloc corresponen
+        }
+    }
+
+    public List<String> getTreeInOrder() {
+        List<String> seq = new ArrayList<String>();
+
+    }
+
+    private void InOrder(TreeNode node, List<String> seq) {
+        if (node == null) return;
+        else {
+            InOrder(node.getLeftNode());
+            seq.add(node.getData());
+            InOrder(node.getRightNode());
+        }
+    }
+
+}
+
+class TreeNode {
+    private String data;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+
+    public TreeNode(String data) {
+        this.data = data;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
     }
 }

09d4e31acdffc98fcba572eb135070670bffcd3f
polfr <pol.fradera@estudiantat.upc.edu>
2022-11-06 22:52:25 +0100

    funció crea arbre

--- src/domini/datatypes/Tree.java
+++ src/domini/datatypes/Tree.java
@@ -16,6 +16,55 @@ public class Tree {
         }
     }
 
+    public TreeNode construir_arbre(List<String> exp) {
+        Map<String, Integer> prio = new HashMap<>();
+
+        prio.put("(", 1);
+        prio.put("!", 2);
+        prio.put("&", 3);
+        prio.put("|", 4);
+
+        Deque<String> ops = new ArrayDeque<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < exp.size(); i++) {
+            String s = exp.get(i);
+            if (s == "(") ops.push(s);
+            else if (s == ")") {
+                while (ops.peek() != "(") {
+                    combine(ops, stack);
+                }
+                // pop left "("
+                ops.pop();
+            }
+
+            else if (s == "&" || s == "!" || s == "|"){
+                while (!ops.isEmpty() && prio.get(ops.peek()) >= prio.get(s)) { //
+                    combine(ops, stack);
+                }
+
+                ops.push(s);
+            }
+            else stack.push(new TreeNode(s));
+        }
+
+        while (stack.size() > 1) {
+            combine(ops, stack);
+        }
+
+        return stack.peek();
+    }
+
+        private void combine(Deque<String> ops, Deque<TreeNode> stack) {
+            TreeNode root = new TreeNode(ops.pop());
+            // right first, then left
+            root.rightNode = stack.pop();
+            root.leftNode = stack.pop();
+
+            stack.push(root);
+        }
+    }
+
     public List<String> getTreeInOrder() {
         List<String> seq = new ArrayList<String>();
 
@@ -24,18 +73,18 @@ public class Tree {
     private void InOrder(TreeNode node, List<String> seq) {
         if (node == null) return;
         else {
-            InOrder(node.getLeftNode());
-            seq.add(node.getData());
-            InOrder(node.getRightNode());
+            InOrder(node.leftNode);
+            seq.add(node.data);
+            InOrder(node.rightNode);
         }
     }
 
 }
 
 class TreeNode {
-    private String data;
-    private TreeNode leftNode;
-    private TreeNode rightNode;
+    public String data;
+    public TreeNode leftNode;
+    public TreeNode rightNode;
 
     public TreeNode(String data) {
         this.data = data;
@@ -43,7 +92,7 @@ class TreeNode {
         this.rightNode = null;
     }
 
-    public String getData() {
+    /*public String getData() {
         return data;
     }
 
@@ -64,5 +113,5 @@ class TreeNode {
     }
     public void setRightNode(TreeNode rightNode) {
         this.rightNode = rightNode;
-    }
+    }*/
 }

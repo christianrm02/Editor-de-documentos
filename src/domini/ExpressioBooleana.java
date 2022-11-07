f04b9b0f5758a5dfe1617dcf0b1e518b096a509b
polfr <pol.fradera@estudiantat.upc.edu>
2022-11-07 11:49:01 +0100

    canvis a expBool

--- src/domini/ExpressioBooleana.java
+++ src/domini/ExpressioBooleana.java
@@ -7,6 +7,8 @@ public class ExpressioBooleana {
     private String exp;
     private Tree expA;
 
+    private ArrayList<String> llista;
+
     //Constructora
     public ExpressioBooleana (String nom, String exp) {
         this.nom = nom;
@@ -21,6 +23,38 @@ public class ExpressioBooleana {
 
     private void crearArbre() {
 
+    }
+/*
+    private boolean operador(char c) {
+        if (c <= 32 || c >= 35 && )
+    }*/
+
+    private void crearLlista(String s) throws Exception {
+        List<String> paraules = new ArrayList<String>();
+        boolean clau_oberta = false;
+        boolean espai = true;
+        int parent_obert = 0;
+        String s1 = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (espai && s.charAt(i) == ' ') throw new Exception();
+            if (clau_oberta) {
+                if (s.charAt(i) != ' ' && s.charAt(i) != '}') {
+                    espai = false;
+                    s1+=s.charAt(i);
+                }
+                else if (s.charAt(i) == '}') {
+                    llista.add(s1);
+                    clau_oberta = false;
+                }
+                else {
+                    espai = true;
+                    llista.add(s1);
+                    llista.add("&");
+                    s1 = "";
+                }
+            }
+            else if (s.charAt(i) == '{') clau_oberta = true;
+        }
     }
 
     //Getters

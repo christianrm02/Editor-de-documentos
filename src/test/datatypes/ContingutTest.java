175bd4115db8f4edf57e927a5831c8be1bd504ab
mroman <marc.roman.quilez@estudiantat.upc.edu>
2022-11-10 15:50:33 +0100

    Petits canvis ContingutTest

--- src/test/datatypes/ContingutTest.java
+++ src/test/datatypes/ContingutTest.java
@@ -21,40 +21,39 @@ public class ContingutTest {
     public void testConstructores() {
         Contingut conttest = new Contingut();
         assertEquals(new ArrayList<String>(), conttest.getFrases());
-
-        Contingut conttest2 = new Contingut("Hola, em dic Marc. Com estàs?\n Jo bé! Bueno adéeeu");
+        Contingut conttest2 = new Contingut("Hola, em dic Marc. Com estàs?\nJo bé! Bueno adéeeu");
         List<String> l = new ArrayList<String>();
-        l.add("Hola, em dic Marc."); l.add(" Com estàs?"); l.add("\n"); l.add(" Jo bé!"); l.add(" Bueno adéeeu");
+        l.add("Hola, em dic Marc."); l.add("Com estàs?"); l.add("\n"); l.add("Jo bé!"); l.add("Bueno adéeeu");
         assertEquals(l, conttest2.getFrases());
     }
 
     @Test
     public void getFrases() {
-        Contingut conttest = new Contingut("Hola, em dic Marc. Com estàs?\n Jo bé! Bueno adéeeu");
+        Contingut conttest = new Contingut("Hola, em dic Marc. Com estàs?\nJo bé! Bueno adéeeu");
         List<String> l = new ArrayList<String>();
-        l.add("Hola, em dic Marc."); l.add(" Com estàs?"); l.add("\n"); l.add(" Jo bé!"); l.add(" Bueno adéeeu");
+        l.add("Hola, em dic Marc."); l.add("Com estàs?"); l.add("\n"); l.add("Jo bé!"); l.add("Bueno adéeeu");
         assertEquals(l, conttest.getFrases());
     }
 
     @Test
     public void actualitzaContingut() {
         Contingut conttest = new Contingut();
-        conttest.actualitzaContingut("Hola, em dic Marc. Com estàs?\n Jo bé! Bueno adéeeu");
+        conttest.actualitzaContingut("Hola, em dic Marc. Com estàs?\nJo bé! Bueno adéeeu");
         List<String> l = new ArrayList<String>();
         l.add("Hola, em dic Marc.");
-        l.add(" Com estàs?");
+        l.add("Com estàs?");
         l.add("\n");
-        l.add(" Jo bé!");
-        l.add(" Bueno adéeeu");
+        l.add("Jo bé!");
+        l.add("Bueno adéeeu");
         assertEquals(l, conttest.getFrases());
-        conttest.actualitzaContingut("Hola, em dic Marc. Com estàs?\n Jo bé! Bueno adéeeu");
+        conttest.actualitzaContingut("Hola, em dic Marc. Com estàs?\nJo bé! Bueno adéeeu");
         assertEquals(l, conttest.getFrases());
-        conttest.actualitzaContingut("Bon dia, com vas? Ara ve el mundial, fins cap d'any no torna a jugar el Barça. Vaja\n Què hi farem");
+        conttest.actualitzaContingut("Bon dia, com vas? Ara ve el mundial, fins cap d'any no torna a jugar el Barça. Vaja\nQuè hi farem");
         List<String> l2 = new ArrayList<String>();
         l2.add("Bon dia, com vas?");
-        l2.add(" Ara ve el mundial, fins cap d'any no torna a jugar el Barça.");
-        l2.add(" Vaja\n");
-        l2.add(" Què hi farem");
+        l2.add("Ara ve el mundial, fins cap d'any no torna a jugar el Barça.");
+        l2.add("Vaja\n");
+        l2.add("Què hi farem");
         assertEquals(l2, conttest.getFrases());
     }
 }

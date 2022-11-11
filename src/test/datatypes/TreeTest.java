1a53a1bfefff69b3044fc588861cea08af3bd69f
polfr <pol.fradera@estudiantat.upc.edu>
2022-11-11 11:46:27 +0100

    classe creada:TreeTest

--- src/test/datatypes/TreeTest.java
+++ src/test/datatypes/TreeTest.java
@@ -2,12 +2,29 @@ package datatypes;
 
 import org.junit.Test;
 
+import java.util.*;
+
+import static datatypes.Tree.InOrder;
 import static org.junit.Assert.*;
 
 public class TreeTest {
 
     @Test
     public void expressionTree() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("&");
+        list.add("c");
+        list.add("&");
+        list.add("d");
+        list.add("e");
+        list.add("|");
+        list.add("&");
+        list.add("h");
+        list.add("&");
+        Tree tree = new Tree(list);
+        InOrder(tree.root);
     }
 
     @Test

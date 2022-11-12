84dd40946c233c2d73d35f03f95630aaa60fefd2
mroman <marc.roman.quilez@estudiantat.upc.edu>
2022-11-12 17:11:57 +0100

    canvis

--- src/drivers/DriverCtrlDomini.java
+++ src/drivers/DriverCtrlDomini.java
@@ -83,6 +83,7 @@ public class DriverCtrlDomini {
             String ent = entrada.nextLine();
             for (int i = 0; i < min(10, nDocuments) && !ent.equals("0"); ++i) {
                 docs.add(m.get(Integer.valueOf(ent)));
+                ent = entrada.nextLine();
             }
             if (docs.size() == 0) System.out.println("No has seleccionat cap document a esborrar.");
             else {

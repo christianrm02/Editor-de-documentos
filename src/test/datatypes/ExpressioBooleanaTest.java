d578f4e0566a6e93cfc2a36770837bb0f2b5830b
polfr <pol.fradera@estudiantat.upc.edu>
2022-11-12 18:52:05 +0100

    test de l'arbre acabat

--- src/test/datatypes/ExpressioBooleanaTest.java
+++ src/test/datatypes/ExpressioBooleanaTest.java
@@ -34,21 +34,40 @@ public class ExpressioBooleanaTest {
                 /         \             /
                &(3)         |(4)     joan(5)
            /     \      /         \
-         &(6)  p3(7) "hola adéu"(8) pep(9)
-          /   \
-       p1(10)  p2(11)
+         &(6)  p3(7) hola adéu(8) pep(9)
+        /   \
+   p1(10)  p2(11)
 */
     @Test
     public void getExpA() {
         ExpressioBooleana expTest = new ExpressioBooleana("Expressió 1", "{p1 p2 p3} & (\"hola adéu\" | pep) & !joan");
         Tree arbre = expTest.getExpA();
-        assertEquals("&", arbre.root.data);
-        TreeNode fillesq = arbre.root.leftNode;
-        TreeNode filldre = arbre.root.rightNode;
-        assertEquals("!", filldre.data);
-        assertEquals("&", fillesq.data);
-        TreeNode filldret = filldre.rightNode;
-        assertEquals(null, filldret);
+
+        TreeNode node0 = arbre.root;
+        TreeNode node1 = node0.leftNode;
+        TreeNode node2 = node0.rightNode;
+        TreeNode node3 = node1.leftNode;
+        TreeNode node4 = node1.rightNode;
+        TreeNode node5 = node2.leftNode;
+        TreeNode node6 = node3.leftNode;
+        TreeNode node7 = node3.rightNode;
+        TreeNode node8 = node4.leftNode;
+        TreeNode node9 = node4.rightNode;
+        TreeNode node10 = node6.leftNode;
+        TreeNode node11 = node6.rightNode;
+
+        assertEquals("&", node0.data);
+        assertEquals("&", node1.data);
+        assertEquals("!", node2.data);
+        assertEquals("&", node3.data);
+        assertEquals("|", node4.data);
+        assertEquals("joan", node5.data);
+        assertEquals("&", node6.data);
+        assertEquals("p3", node7.data);
+        assertEquals("hola adéu", node8.data);
+        assertEquals("pep", node9.data);
+        assertEquals("p1", node10.data);
+        assertEquals("p2", node11.data);
 
     }
 }

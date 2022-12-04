1c9edda3328cc35b116108232efe640058e09c99
Eric Ryhr <59489835+ericryhr@users.noreply.github.com>
2022-12-04 11:58:53 +0100

    fix trie + gestor indexs

---
+++ src/persistencia/GestorDocuments.java
@@ -0,0 +1,20 @@
+package persistencia;
+
+public class GestorDocuments {
+    
+    public void CarregaDocument(String path) {
+
+    }
+
+    public void ExportaDocument(String path) {
+
+    }
+
+    public void ObrirDocument(String autor, String titol) {
+
+    }
+
+    public void TancarDocument() {
+
+    }
+}
--- src/persistencia/GestorIndexs.java
+++ src/persistencia/GestorIndexs.java
@@ -1,13 +1,37 @@
 package persistencia;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class GestorIndexs {
     
-
-    public void ImportarIndexs() {
-
+    public byte[] ImportarIndexs() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("./appdata/indexs/index");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            byte[] info = (byte[]) objectInputStream.readObject();
+            objectInputStream.close();
+            return info;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public void ExportarIndexs() {
-        
+    public void ExportarIndexs(byte[] info) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("./appdata/indexs/index");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(info);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }

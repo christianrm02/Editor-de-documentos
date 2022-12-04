4e0745e7614cdf0739b2bef2c360fd39e2bc954f
christian.rivero <christian.rivero@estudiantat.upc.edu>
2022-12-04 18:16:42 +0100

    actualització main vista

--- src/presentacio/Main.java
+++ src/presentacio/Main.java
@@ -1,31 +1,33 @@
 package presentacio;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
 
 public class Main extends JFrame {
     private JPanel panel1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
+    private JButton creaButton;
+    private JButton importaButton;
+    private JButton ajudaButton;
     private JButton xButton;
-    private JList list1;
-    private JList list2;
-
+    private JPanel tablePanel;
+    private JButton button1;
+    private JButton busquedaButton;
 
 
     public Main(String title) {
-        super(title);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(panel1);
-        this.pack();
+        setTitle("Editor de textos");
+        setSize(1000, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
 
-        list1.addMouseListener(new MouseAdapter() {
+        /*list1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 2) {
@@ -42,11 +44,90 @@ public class Main extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
+        });*/
+
+
+        JLabel moreOptions = new JLabel();
+        Icon iconMoreOptions = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/icons/moreOptions.png")));
+        moreOptions.setIcon(iconMoreOptions);
+
+        String[] colums = {"Titols", "Autors", "Darrera modificació", ""};
+        Object[][] titols = {
+                {"Dia", "Pep", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Day", "Pepe", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Rio", "Romero", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Camino", "Alex", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Dia", "Zacarias", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"SD", "Juan", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"La hostia", "JAGGER", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Illo no veas", "Illojuan", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Siempre tipo fuego", "Oak", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Adelante Pikachu", "Ash", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Tornado de fuego", "Axel", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Mano mágica", "Marc", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Lo tengo!", "Layton", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Ay ay ay", "Luke", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Roooar", "Luxray", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Yo q sé", "Yo", LocalDate.now() + " " + LocalTime.now(), moreOptions},
+                {"Lápiz", "Antes", LocalDate.of(1972, 2, 23) + " " + LocalTime.of(20, 01, 15, 0003), moreOptions},
+                {"Vida", "Después", LocalDate.of(1972, 2, 23) + " " + LocalTime.of(20, 03, 15, 0003), moreOptions}};
+        DefaultTableModel tableModel = new DefaultTableModel(titols, colums);
+        JTable documents = new JTable(tableModel);
+        documents.setAutoResizeMode(5);
+        documents.setAutoCreateRowSorter(true);
+        tablePanel.setLayout(new BorderLayout());
+        JScrollPane tableScroll = new JScrollPane(documents);
+        documents.setFillsViewportHeight(true);
+        tablePanel.add(tableScroll, BorderLayout.CENTER);
+
+
+        JMenu menu;
+        JMenuItem i1, i2, i3;
+        JMenuBar mb = new JMenuBar();
+        menu = new JMenu("Búsqueda");
+        i1=new JMenuItem("Llistar títols autor");
+        i2=new JMenuItem("Llistar autors prefix");
+        i3=new JMenuItem("Cerca per rellevància");
+        menu.add(i1); menu.add(i2); menu.add(i3);
+        mb.add(menu);
+        JPopupMenu popBusqueda = new JPopupMenu();
+        popBusqueda.add(i1); popBusqueda.add(i2); popBusqueda.add(i3);
+
+        busquedaButton.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                popBusqueda.show(e.getComponent(), busquedaButton.getX()+busquedaButton.getWidth(), e.getY()-30);
+            }
         });
+
+        validate();
+
     }
 
     public static void main(String[] args) {
         JFrame view = new Main("Editor de textos");
-        view.setVisible(true);
     }
 }
+
+/*
+String[] colums = {"Titols", "Autors", "Darrera modificació"};
+        Object[][] titols = {
+                {"Dia", "Pep", LocalDateTime.now()},
+                {"Day", "Pepe"},
+                {"Rio", "Romero"},
+                {"Camino", "Alex"},
+                {"Dia", "Zacarias"},
+                {"SD", "Juan"},
+                {"La hostia", "JAGGER"},
+                {"Illo no veas", "Illojuan"},
+                {"Siempre tipo fuego", "Oak"},
+                {"Adelante Pikachu", "Ash"},
+                {"Tornado de fuego", "Axel"},
+                {"Mano mágica", "Marc"},
+                {"Lo tengo!", "Layton"},
+                {"Ay ay ay", "Luke"},
+                {"Roooar", "Luxray"},
+                {"Yo q sé", "Yo"},
+                {"Lápiz", "Boli"},
+                {"Vida", "Alex"}};
+*/
+

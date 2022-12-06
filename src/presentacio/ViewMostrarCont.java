7171f47392134101a2164f5024d45f7bbd56a3e7
mroman <marc.roman.quilez@estudiantat.upc.edu>
2022-12-06 11:29:35 +0100

    Moltes coses fetes

--- src/presentacio/ViewMostrarCont.java
+++ src/presentacio/ViewMostrarCont.java
@@ -3,14 +3,14 @@ package presentacio;
 import javax.swing.*;
 
 public class ViewMostrarCont extends ViewEditar {
-    public ViewMostrarCont(String title, String t, String a, String cont) {
-        super(title, t, a, cont);
+    public ViewMostrarCont(CtrlPresentacio cp, String t, String a, String cont) {
+        super(cp, t, a, cont);
         textPane1.setEditable(false);
         desarButton.setVisible(false);
         exportarButton.setVisible(false);
     }
 
     public static void main(String[] args) {
-        JFrame view = new ViewMostrarCont("Editor de textos", "Hola q tal", "Paco", "Hola q tal, com estàs?");
+        JFrame view = new ViewMostrarCont(new CtrlPresentacio(), "Hola q tal", "Paco", "Hola q tal, com estàs?");
     }
 }

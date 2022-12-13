bd173bb00c974ea4f7882a51baef8042f6270982
polfr <pol.fradera@estudiantat.upc.edu>
2022-12-13 19:48:03 +0100

    funció carrega expB acabada

--- src/persistencia/GestorExpBooleanes.java
+++ src/persistencia/GestorExpBooleanes.java
@@ -1,26 +1,42 @@
 package persistencia;
 
+import transversal.Pair;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GestorExpBooleanes {
-    public String[] CarregarExpB(String path) throws IOException {
-        String nom = path.substring(path.lastIndexOf("/")+1);
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        String exp = reader.readLine();
-        reader.close();
-        return new String[]{nom, exp};
+    public static List<Pair<String, String>> CarregarExpB() throws IOException {
+        List<Pair<String, String>> exps = new ArrayList<>();
+
+        String dirPath = "./appdata/expressions/";
+        File carpeta = new File(dirPath);
+        File[] llistaFitxers = carpeta.listFiles();
+        for (int i = 0; i < llistaFitxers.length; ++i) {
+            String nom = llistaFitxers[i].getName();
+            BufferedReader reader = new BufferedReader(new FileReader(dirPath.concat(nom)));
+            String exp = reader.readLine();
+            reader.close();
+            Pair<String, String> p = new Pair<>();
+            p.x = nom;
+            p.y = exp;
+            exps.add(p);
+        }
+        return exps;
     }
 
-    public void GuardarExpB(String nom, String exp) throws IOException {
+    public static void GuardarExpB(List<Pair<String, String>> exps) throws IOException {
         String dirPath = "./appdata/expressions/";
         Files.createDirectories(Paths.get(dirPath));
-        FileOutputStream fileOutputStream = new FileOutputStream(dirPath.concat(nom));
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-        objectOutputStream.writeObject(exp);
-        objectOutputStream.flush();
-        objectOutputStream.close();
+        for (int i = 0; i < exps.size(); ++i) {
+            FileOutputStream fileOutputStream = new FileOutputStream(dirPath.concat(exps.get(i).x));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(exps.get(i).y);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        }
     }
 }

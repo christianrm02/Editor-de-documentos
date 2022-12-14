60b5ce634596988fdcd1ea21c1fe41831d2396be
polfr <pol.fradera@estudiantat.upc.edu>
2022-12-14 13:05:26 +0100

    canvis

--- src/persistencia/GestorExpBooleanes.java
+++ src/persistencia/GestorExpBooleanes.java
@@ -12,6 +12,52 @@ import java.util.List;
 
 public class GestorExpBooleanes {
     public static List<Pair<String, String>> CarregarExpB() throws IOException {
+        try {
+            List<Pair<String, String>> exps = new ArrayList<>();
+
+            String dirPath = "./appdata/expressions/";
+            File carpeta = new File(dirPath);
+            File[] llistaFitxers = carpeta.listFiles();
+            for (int i = 0; i < llistaFitxers.length; ++i) {
+                String nom = llistaFitxers[i].getName();
+                FileInputStream fileInputStream = new FileInputStream(dirPath.concat(nom));
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                String exp = (String) objectInputStream.readObject();
+                objectInputStream.close();
+                Pair<String, String> p = new Pair<>();
+                p.x = nom;
+                p.y = exp;
+                exps.add(p);
+            }
+            return exps;
+        }
+        catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+   public static void GuardarExpB(List<Pair<String, String>> exps) throws IOException {
+        String dirPath = "./appdata/expressions/";
+        Files.createDirectories(Paths.get(dirPath));
+        for (int i = 0; i < exps.size(); ++i) {
+            FileOutputStream fileOutputStream = new FileOutputStream(dirPath.concat(exps.get(i).x));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(exps.get(i).y);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        }
+    }
+}
+/*    public static void GuardarExpB(List<Pair<String, String>> exps) throws IOException {
+        String dirPath = "./appdata/expressions/";
+        Files.createDirectories(Paths.get(dirPath));
+        for (int i = 0; i < exps.size(); ++i) {
+            Files.writeString(Path.of(dirPath.concat(exps.get(i).x)), exps.get(i).y, StandardCharsets.UTF_8);
+        }
+    }*/
+
+/*    public static List<Pair<String, String>> CarregarExpB() throws IOException {
         List<Pair<String, String>> exps = new ArrayList<>();
 
         String dirPath = "./appdata/expressions/";
@@ -28,25 +74,5 @@ public class GestorExpBooleanes {
             exps.add(p);
         }
         return exps;
-    }
-
-    public static void GuardarExpB(List<Pair<String, String>> exps) throws IOException {
-        String dirPath = "./appdata/expressions/";
-        Files.createDirectories(Paths.get(dirPath));
-        for (int i = 0; i < exps.size(); ++i) {
-            Files.writeString(Path.of(dirPath.concat(exps.get(i).x)), exps.get(i).y, StandardCharsets.UTF_8);
-        }
-    }
-}
-
-/*    public static void GuardarExpB(List<Pair<String, String>> exps) throws IOException {
-        String dirPath = "./appdata/expressions/";
-        Files.createDirectories(Paths.get(dirPath));
-        for (int i = 0; i < exps.size(); ++i) {
-            FileOutputStream fileOutputStream = new FileOutputStream(dirPath.concat(exps.get(i).x));
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(exps.get(i).y);
-            objectOutputStream.flush();
-            objectOutputStream.close();
-        }
     }*/
+

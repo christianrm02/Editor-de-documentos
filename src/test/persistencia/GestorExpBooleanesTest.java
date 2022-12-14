fa67625a5e332bb44ca02a2f16a6cdbcdf2390cd
polfr <pol.fradera@estudiantat.upc.edu>
2022-12-14 12:07:38 +0100

    petits canvis

---
+++ src/test/persistencia/GestorExpBooleanesTest.java
@@ -0,0 +1,33 @@
+package persistencia;
+
+import org.junit.Test;
+import transversal.Pair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class GestorExpBooleanesTest {
+
+    @Test
+    public void carregarExpB() {
+    }
+
+    @Test
+    public void guardarExpB() throws IOException {
+        List<Pair<String, String>> exps = new ArrayList<>();
+        Pair<String, String> p = new Pair<>();
+        p.x = "primera";
+        p.y = "hola & adeu";
+        exps.add(p);
+
+        Pair<String, String> p2 = new Pair<>();
+        p2.x = "segona";
+        p2.y = "hola & !adeu | \"bon dia\"";
+        exps.add(p2);
+
+        GestorExpBooleanes.GuardarExpB(exps);
+    }
+}

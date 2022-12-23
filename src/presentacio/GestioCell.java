9b746701492564c10aebc780b160531134ad461a
christian.rivero <christian.rivero@estudiantat.upc.edu>
2022-12-23 19:12:57 +0100

    mejoras

--- src/presentacio/GestioCell.java
+++ src/presentacio/GestioCell.java
@@ -52,7 +52,8 @@ public class GestioCell extends DefaultTableCellRenderer {
 
         if (selected) {
             this.setBackground(colorFondo);
-        } else {
+        }
+        else {
             this.setBackground(Color.white);
         }
 
